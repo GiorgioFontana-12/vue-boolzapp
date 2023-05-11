6f516ee8beae27aee1a41d4b667e9a67827fffce
Giorgio Fontana <giorgiofontanaj@gmail.com>
2023-05-11 23:05:07 +0200

    milestone 1 completed

--- js/main.js
+++ js/main.js
@@ -4,14 +4,14 @@ createApp({
     data() {
         return{
             chats: [
-                {name: "michele", lastAccess: "x:x", img:"./img/avatar_1.jpg"},
-                {name: "michele", lastAccess: "x:x", img:"./img/avatar_2.jpg"},
-                {name: "michele", lastAccess: "x:x", img:"./img/avatar_3.jpg"},
-                {name: "michele", lastAccess: "x:x", img:"./img/avatar_4.jpg"},
-                {name: "michele", lastAccess: "x:x", img:"./img/avatar_5.jpg"},
-                {name: "michele", lastAccess: "x:x", img:"./img/avatar_6.jpg"},
-                {name: "michele", lastAccess: "x:x", img:"./img/avatar_7.jpg"},
-                {name: "michele", lastAccess: "x:x", img:"./img/avatar_8.jpg"},
+                {name: "Michele", lastAccess: "x:x", img:"./img/avatar_1.jpg"},
+                {name: "Fabio", lastAccess: "x:x", img:"./img/avatar_2.jpg"},
+                {name: "Samuele", lastAccess: "x:x", img:"./img/avatar_3.jpg"},
+                {name: "Alessanfro B.", lastAccess: "x:x", img:"./img/avatar_4.jpg"},
+                {name: "Alessandro L.", lastAccess: "x:x", img:"./img/avatar_5.jpg"},
+                {name: "Claudia", lastAccess: "x:x", img:"./img/avatar_6.jpg"},
+                {name: "Federico", lastAccess: "x:x", img:"./img/avatar_7.jpg"},
+                {name: "Davide", lastAccess: "x:x", img:"./img/avatar_8.jpg"},
             ]
         }
     },
