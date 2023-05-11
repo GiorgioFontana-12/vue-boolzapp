927c979a3ddc2df9da8b01b27640f5dfd2e8306d
Giorgio Fontana <giorgiofontanaj@gmail.com>
2023-05-11 20:35:41 +0200

    feat scroll chats

--- js/main.js
+++ js/main.js
@@ -2,7 +2,18 @@ const { createApp } = Vue
 
 createApp({
     data() {
-
+        return{
+            chats: [
+                {name: "michele", lastAccess: "x:x", img:"./img/avatar_1.jpg"},
+                {name: "michele", lastAccess: "x:x", img:"./img/avatar_2.jpg"},
+                {name: "michele", lastAccess: "x:x", img:"./img/avatar_3.jpg"},
+                {name: "michele", lastAccess: "x:x", img:"./img/avatar_4.jpg"},
+                {name: "michele", lastAccess: "x:x", img:"./img/avatar_5.jpg"},
+                {name: "michele", lastAccess: "x:x", img:"./img/avatar_6.jpg"},
+                {name: "michele", lastAccess: "x:x", img:"./img/avatar_7.jpg"},
+                {name: "michele", lastAccess: "x:x", img:"./img/avatar_8.jpg"},
+            ]
+        }
     },
 
     methods: {
